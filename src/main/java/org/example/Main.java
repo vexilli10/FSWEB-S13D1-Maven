3acd022de5583708b0325a05203bf6d6f2d48f55
vexilli10 <taylanakardas@gmail.com>
2025-04-22 16:10:14 +0300

    sprint 13 project 1 completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,34 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+        return isBarking && (clock < 8 || clock >= 20);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge >= 13 && firstAge <= 19)
+                || (secondAge >= 13 && secondAge <= 19)
+                || (thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int upperLimit = isSummer ? 45 : 35;
+        return temp >= 25 && temp <= upperLimit;
     }
 
     public static double area(double width, double height) {
-       
+        if (width < 0 || height < 0) {
+            return -1;
+        }
+        return width * height;
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
     }
 }
